2a741455100c4b478e15b8bfc0b36be65fada3aa
ryuda <ryuda0101@naver.com>
2022-11-17 18:06:25 +0900

    css 디테일 작업중

--- public/js/ham_menu.js
+++ public/js/ham_menu.js
@@ -1,18 +1,15 @@
+let blur_bg = document.querySelector("header .center .blur_bg");
 let ham_btn = document.querySelector("header .center .mobile_header .btn");
-let close_btn = document.querySelector("header .center .mobile_header .right_menu i"); 
+let ham_btns = document.querySelectorAll("header .center .mobile_header .btn span");
 let right_gnb = document.querySelector("header .center .mobile_header .right_menu");
 
 window.addEventListener("resize",() => {
-    ham_btn.addEventListener("click",() => {
-        mediaCheck();
-    });
+    ham_gnb_act();
 });
 
+
 ham_btn.addEventListener("click",() => {
-    mediaCheck();
-});
-close_btn.addEventListener("click",() => {
-    right_gnb.style.width = 0;
+    ham_gnb_act();
 });
 
 let mediaCheck = () => {
@@ -26,3 +23,33 @@ let mediaCheck = () => {
         right_gnb.style.width = "70vw";
     }
 }
+
+let ham_gnb_act = () => {
+    if (right_gnb.offsetWidth == 0) {
+        for(let i = 0; i <ham_btns.length; i++) {
+            ham_btns[i].classList.add("on");
+        }
+        blur_bg.classList.add("on");
+        mediaCheck();
+    }
+    else {
+        for(let i = 0; i <ham_btns.length; i++) {
+            ham_btns[i].classList.remove("on");
+        }
+        right_gnb.style.width = 0;
+        blur_bg.classList.remove("on");
+    }
+}
+
+
+let right_gnb_menu = document.querySelectorAll("header .center .mobile_header .right_menu .ham_gnb >li");
+
+right_gnb_menu.forEach((item, index) => {
+    item.addEventListener("click", (event) => {
+        right_gnb_menu.forEach((item, index) => {
+            item.querySelector(".ham_sub_gnb").style.height = "0";
+        });
+        let length_num = item.querySelectorAll("ul li").length;
+        item.querySelector(".ham_sub_gnb").style.height = 40 * length_num + "px";
+    });
+});
